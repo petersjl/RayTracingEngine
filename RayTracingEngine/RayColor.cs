c80de3b0017978e949b999123098d0851da6c109
Joseph Peters <petersjl@rose-hulman.edu>
2024-11-21 16:45:57 -0500

    Add ray colors

---
+++ RayTracingEngine/RayColor.cs
@@ -0,0 +1,23 @@
+﻿using RayTuples;
+
+namespace RayColors;
+
+public class RayColor : RayTuple
+{
+    public RayColor(double red, double green, double blue) : this(new RayTuple(red, green, blue, 0.0))
+    {
+    }
+
+    private RayColor(RayTuple tuple) : base(tuple.X, tuple.Y, tuple.Z, tuple.W)
+    {
+    }
+
+    public double Red => X;
+    public double Green => Y;
+    public double Blue => Z;
+
+    public static RayColor operator +(RayColor left, RayColor right)
+    {
+        return new RayColor((RayTuple)left + right);
+    }
+}
---
+++ RayTracingTests/ColorTest.cs
@@ -0,0 +1,28 @@
+﻿using RayColors;
+
+namespace RayTracingTests;
+
+[TestFixture]
+public class ColorTest
+{
+    [Test]
+    public void ShouldBeCreatedWithConstructor()
+    {
+        var color = new RayColor(-0.5, 0.4, 1.7);
+        Assert.That(color.Red, Is.EqualTo(-0.5));
+        Assert.That(color.Green, Is.EqualTo(0.4));
+        Assert.That(color.Blue, Is.EqualTo(1.7));
+    }
+
+    [Test]
+    public void ShouldAddColors()
+    {
+        var c1 = new RayColor(0.9, 0.6, 0.75);
+        var c2 = new RayColor(0.7, 0.1, 0.25);
+        var c3 = c1 + c2;
+
+        Assert.That(c3.Red, Is.EqualTo(1.6));
+        Assert.That(c3.Green, Is.EqualTo(0.7));
+        Assert.That(c3.Blue, Is.EqualTo(1.0));
+    }
+}
