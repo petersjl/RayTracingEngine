b7f237f85087fbdc3b2e44b0cd58094b0f1072fe
Joseph Peters <petersjl@rose-hulman.edu>
2024-11-26 11:52:57 -0500

    Add canvas

---
+++ RayTracingEngine/Canvas.cs
@@ -0,0 +1,42 @@
+﻿using RayColors;
+
+namespace RayTracingEngine;
+
+public class Canvas
+{
+    private readonly RayColor[][] _pixelMap;
+
+    public Canvas(int height, int width)
+    {
+        Height = height;
+        Width = width;
+
+        _pixelMap = new RayColor[height][];
+        for (var i = 0; i < height; i++)
+        {
+            var line = new RayColor[width];
+            for (var j = 0; j < width; j++) line[j] = new RayColor(0, 0, 0);
+            _pixelMap[i] = line;
+        }
+    }
+
+    public int Height { get; }
+    public int Width { get; }
+
+    public RayColor[] this[int i] => _pixelMap[i];
+
+    public IEnumerable<RayColor> PixelStream
+    {
+        get
+        {
+            return _pixelMap.SelectMany(line =>
+                line.Select(pixel => pixel)
+            );
+        }
+    }
+
+    public void Write(int row, int col, RayColor color)
+    {
+        _pixelMap[row][col] = color;
+    }
+}
---
+++ RayTracingTests/CanvasTest.cs
@@ -0,0 +1,37 @@
+﻿using RayColors;
+using RayTracingEngine;
+
+namespace RayTracingTests;
+
+public class CanvasTest
+{
+    private readonly RayColor black = new(0, 0, 0);
+
+    [Test]
+    public void CreateCanvas()
+    {
+        var c = new Canvas(10, 20);
+
+        Assert.That(c.Height, Is.EqualTo(10));
+        Assert.That(c.Width, Is.EqualTo(20));
+        var count = 0;
+        foreach (var pixel in c.PixelStream)
+        {
+            count++;
+            Assert.That(pixel, Is.EqualTo(black));
+        }
+
+        Assert.That(count, Is.EqualTo(200));
+    }
+
+    [Test]
+    public void WriteToCanvas()
+    {
+        var canvas = new Canvas(10, 20);
+        var red = new RayColor(1, 0, 0);
+
+        canvas.Write(2, 3, red);
+
+        Assert.That(canvas[2][3], Is.EqualTo(red));
+    }
+}
