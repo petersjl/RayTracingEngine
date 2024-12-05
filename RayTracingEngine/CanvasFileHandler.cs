4e7881736f94db809e8b69aebbe5d64ee8c80d0c
Joseph Peters <petersjl@rose-hulman.edu>
2024-12-05 15:11:04 -0500

    Add Canvas handler

---
+++ RayTracingEngine/CanvasFileHandler.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using RayColors;
+
+namespace RayTracingEngine;
+
+public static class CanvasFileHandler
+{
+    public static string toPPM(Canvas canvas)
+    {
+        var builder = new StringBuilder();
+        builder.Append("P3\n")
+            .Append($"{canvas.Width} {canvas.Height}\n")
+            .Append("255\n");
+        for (var row = 0; row < canvas.Height; row++)
+        {
+            var pixels = new List<string>();
+            for (var col = 0; col < canvas.Width; col++) pixels.Add(GetBitString(canvas[row, col]));
+            builder.Append(string.Join(" ", pixels));
+            builder.Append('\n');
+        }
+
+        return builder.ToString();
+    }
+
+    private static string GetBitString(RayColor color)
+    {
+        var red = Convert.ToUInt16(Math.Clamp(color.Red, 0, 1) * 255);
+        var green = Convert.ToUInt16(Math.Clamp(color.Green, 0, 1) * 255);
+        var blue = Convert.ToUInt16(Math.Clamp(color.Blue, 0, 1) * 255);
+        return $"{red} {green} {blue}";
+    }
+}
---
+++ RayTracingTests/CanvasFileHandlerTests.cs
@@ -0,0 +1,65 @@
+﻿using RayColors;
+using RayTracingEngine;
+
+namespace RayTracingTests;
+
+public class CanvasFileHandlerTests
+{
+    [Test]
+    public void CreatePPMHeader()
+    {
+        var canvas = new Canvas(3, 5);
+        var fileContent = CanvasFileHandler.toPPM(canvas);
+
+        var expectedContent = "P3\n5 3\n255\n";
+        Assert.That(fileContent, Does.StartWith(expectedContent));
+    }
+
+    [Test]
+    public void CreatesPixelValues()
+    {
+        var canvas = new Canvas(3, 5);
+        var c1 = new RayColor(1.5, 0, 0);
+        var c2 = new RayColor(0, 0.5, 0);
+        var c3 = new RayColor(-0.5, 0, 1);
+        canvas.Write(0, 0, c1)
+            .Write(1, 2, c2)
+            .Write(2, 4, c3);
+
+        var fileContent = CanvasFileHandler.toPPM(canvas);
+
+        var expectedContent =
+            "255 0 0 0 0 0 0 0 0 0 0 0 0 0 0\n" +
+            "0 0 0 0 0 0 0 128 0 0 0 0 0 0 0\n" +
+            "0 0 0 0 0 0 0 0 0 0 0 0 0 0 255\n";
+
+        Assert.That(fileContent, Does.EndWith(expectedContent));
+    }
+
+    [Test]
+    public void CreatesManyPixelValues()
+    {
+        var canvas = new Canvas(2, 10);
+        var c1 = new RayColor(1, 0.8, 0.6);
+        for (var row = 0; row < canvas.Height; row++)
+        for (var col = 0; col < canvas.Width; col++)
+            canvas.Write(row, col, c1);
+
+        var fileContent = CanvasFileHandler.toPPM(canvas);
+
+        var expectedContent =
+            "255 204 153 255 204 153 255 204 153 255 204 153 255 204 153 255 204 153 255 204 153 255 204 153 255 204 153 255 204 153\n" +
+            "255 204 153 255 204 153 255 204 153 255 204 153 255 204 153 255 204 153 255 204 153 255 204 153 255 204 153 255 204 153\n";
+
+        Assert.That(fileContent, Does.EndWith(expectedContent));
+    }
+
+    [Test]
+    public void EndsWithNewLine()
+    {
+        var canvas = new Canvas(3, 5);
+        var fileContent = CanvasFileHandler.toPPM(canvas);
+
+        Assert.That(fileContent, Does.EndWith("\n"));
+    }
+}
